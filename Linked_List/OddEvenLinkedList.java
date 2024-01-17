3bf8811ecc784526edea66f2c47c77755238cd0b
ketan <ketansondarva2@gmail.com>
2024-01-18 00:00:01 +0530

    added: LC328 - Odd Even Linked List

---
+++ Linked_List/OddEvenLinkedList.java
@@ -0,0 +1,23 @@
+package Linked_List;
+
+
+// 328. Odd Even Linked List
+
+class OddEvenLinkedList {
+    public ListNode oddEvenList(ListNode head) {
+        if (head != null) {
+        
+            ListNode odd = head, even = head.next, evenHead = even; 
+        
+            while (even != null && even.next != null) {
+                odd.next = odd.next.next; 
+                even.next = even.next.next; 
+                odd = odd.next;
+                even = even.next;
+            }
+            odd.next = evenHead; 
+        }
+        return head;
+    }
+}
+
